70c0764cf40e14b72eb9b8d8c039f0fc3ba1ad3b
yubaitw <yubaitw@gmail.com>
2025-09-04 16:40:21 +0800

    src/main.rs: Refactor CLI argument parsing using match for clarity.

--- src/main.rs
+++ src/main.rs
@@ -6,28 +6,47 @@ mod server;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if !generate::check_is_root(String::from("./")) {
+    let current_directory = String::from("./");
+
+    if !generate::check_is_root(current_directory) {
         println!("Error: the current directory is not a Jet project.");
         return;
-    }
-
-    if args.len() == 2 {
-        if args[1] == "build" {
-            commands::build_site();
-        } else if args[1] == "serve" {
-            commands::serve();
-        } else {
-            println!("Error: command error: unknown command \"{}\" for \"jet\"", args[1]);
-        }
-    } else if args.len() == 3 {
-        if args[1] != "create" {
-            println!("Error: command error: unknown command \"{}\" for \"jet\"", args[1]);
-        } else {
-            let article_slug = args[2].clone();
-            let _ = commands::create_article(article_slug);
-        }
     } else {
-        println!("Wrong number of arguments");
+        let argc = args.len();
+
+        if argc >= 2 {
+            let command = args[1].as_str();
+
+            match command {
+                "build" => {
+                    if argc == 2 {
+                        commands::build_site();
+                    }
+                }
+                "serve" => {
+                    if argc == 2 {
+                        commands::serve();
+                    } else {
+                        println!("Wrong number of arguments");
+                    }
+                }
+                "create" => {
+                    if argc == 3 {
+                        let article_slug = args[2].clone();
+                        let _ = commands::create_article(article_slug);
+                    } else {
+                        println!("Wrong number of arguments");
+                    }
+                }
+                _ => {
+                    println!(
+                        "Error: command error: unknown command \"{}\" for \"jet\"",
+                        &command
+                    );
+                }
+            }
+        } else {
+            println!("Wrong number of arguments");
+        }
     }
 }
-
